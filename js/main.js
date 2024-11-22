a0a1c9d575387925e95027214f0ef16ed98bff01
kidirbayevamirxon <amirxonkidirbayev0@gmail.com>
2024-11-22 18:16:20 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,117 @@
+// length
+// 1
+// let txt = 'salom'
+// console.log(txt.length);
+// 2
+// let txt = 'aziz'
+// console.log(txt.length);
+// 3
+// let txt ='aziz aziz azizi'
+// console.log(txt.length);
+// 4
+// let txt = 'naurizzzzzzzzzzzzzzzzzzzzzxzzzzzzzzzzz'
+// console.log(txt.length);
+// 5
+// let txt = 'mexanizmlashtirilganlardan'
+// console.log(txt.length);
+// charAt
+// 1
+// let txt = 'salom dunyo' 
+// console.log(txt.charAt(4));
+// 2
+// let txt = 'salom aziz'
+// console.log(txt.charAt(8));
+// 3
+// let txt = 'mexanizmlashtirish'
+// console.log(txt.charAt(14));
+// 4
+// let txt = 'shuhratttttt'
+// console.log(txt.charAt(0));
+// 5
+// let txt = 'salom woldd'
+// console.log(txt.charAt(5));
+// concat 
+// 1
+// let txt = 'salom dunyo salom dunyoo'
+// let txt2 = 'ssssssssssss'
+// console.log(txt.concat('       ', txt2));
+// 2
+// let txt = 'salom'
+// let txt2 = 'duny0o'
+// console.log(txt2.concat ('   ', txt));
+// 3
+// let txt = 'salom'
+// let txt2 = 'dunyo'
+// console.log(txt.concat ('   ', txt2));
+// 4
+// let txt = 'salom'
+// let txt2 = 'wold'
+// console.log(txt.concat ('   ', txt2));
+// 5
+// let txt = 'Wello'
+// let txt2 = 'dunyo'
+// console.log(txt.concat ('   ', txt2));
+// includes
+//1
+// let txt = 'salom dunyo'
+// console.log(txt.includes('dunyo'));
+//2
+//let txt = 'hello world'
+// console.log(txt.includes('yello'));
+//3
+// let txt = 'salom'
+// console.log(txt.includes('s'));
+//4
+// let ttx = 'sssa'
+// console.log(ttx.includes('e'));
+//5
+// let ttx = 'aziz'
+// console.log(ttx.includes('shuhr at'));
+//index of
+//1
+// let txt = 'salom dunyo'
+// console.log(txt.indexOf("salom"));
+//2
+// let txt = 'salom salom'
+// console.log(txt.indexOf('salom'));
+//3
+// let txt = 'salom dunyo salom'
+// console.log(txt.indexOf('alom'));
+//4
+// let txt = 'dunyo salom'
+// console.log(txt.indexOf('qalom'));
+//5
+// let txt = ' dunyo salom'
+// console.log(txt.indexOf('y'));
+//lastindexof
+//1
+// let txt = 'javascriptjavascript'
+// console.log(txt.lastIndexOf('javascript'));
+//2
+// let txt = 'superman superman'
+// console.log(txt.lastIndexOf('supreme'));
+//3
+// let txt = 'aziz menen aziz'
+// console.log(txt.lastIndexOf('aziz'));
+//4
+// let txt = 'salom dunyo'
+// console.log(txt.lastIndexOf('o'));
+//5
+// let txt = 'salom dunyo'
+// console.log(txt.lastIndexOf('qwery'));
+//slice
+//1
+// let txt = 'HEllo World'
+// console.log(txt.slice(5, 11));
+//2
+// let txt = 'salom dunyo'
+// console.log(txt.slice(6,11));
+//3
+// let txt = 'salom aziz'
+// console.log(txt.slice(0,5));
+//4
+// let txt = 'aziz salom'
+// console.log(txt.slice(0,5));
+//5
+let txt = 'nauriz salom'
+console.log(txt.slice(6,12));
